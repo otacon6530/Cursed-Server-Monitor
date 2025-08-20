5394769bcfcfa21c275cc3e06133cb8050759598
James Stephens <onlineworkmail@gmail.com>
2025-08-19 23:26:13 -0400

    Adding loop to make client app constantly update sql server.

--- ServerMonitorApp/servmon.py
+++ ServerMonitorApp/servmon.py
@@ -5,6 +5,7 @@ import pyodbc
 import platform
 import importlib
 import psutil
+import time
 from metric_modules import get_cpu_usage
 
 # OS specific code imported decided at run
@@ -43,35 +44,37 @@ DB_CONFIG['driver'] = f'{{{sql_server_drivers[0]}}}'
 
 @cli.command()
 def sql():
-    """Query SQL Server"""
-    try:
-        conn = pyodbc.connect(
-            f"DRIVER={DB_CONFIG['driver']};"
-            f"SERVER={DB_CONFIG['server']},{DB_CONFIG['port']};"
-            f"DATABASE={DB_CONFIG['database']};"
-            f"UID={DB_CONFIG['username']};"
-            f"PWD={DB_CONFIG['password']};"
-            "Encrypt=no;"
-        )
-        cursor = conn.cursor()
-        Server = platform.node()
-        RAMUsage = psutil.virtual_memory().percent
-        CPUusage = get_cpu_usage()
-        DiskUsage = psutil.disk_usage('/').percent
+    """Query SQL Server in an infinite loop every 5 seconds"""
+    while True:
+        try:
+            conn = pyodbc.connect(
+                f"DRIVER={DB_CONFIG['driver']};"
+                f"SERVER={DB_CONFIG['server']},{DB_CONFIG['port']};"
+                f"DATABASE={DB_CONFIG['database']};"
+                f"UID={DB_CONFIG['username']};"
+                f"PWD={DB_CONFIG['password']};"
+                "Encrypt=no;"
+            )
+            cursor = conn.cursor()
+            Server = platform.node()
+            RAMUsage = psutil.virtual_memory().percent
+            CPUusage = get_cpu_usage()
+            DiskUsage = psutil.disk_usage('/').percent
 
-        query = (
-            "EXECUTE [dbo].[SetServerMetrics] "
-            "@Server = ?, @RAMUsage = ?, @CPUusage = ?, @DiskUsage = ?;"
-        )
-        params = (Server, RAMUsage, CPUusage, DiskUsage)
+            query = (
+                "EXECUTE [dbo].[SetServerMetrics] "
+                "@Server = ?, @RAMUsage = ?, @CPUusage = ?, @DiskUsage = ?;"
+            )
+            params = (Server, RAMUsage, CPUusage, DiskUsage)
 
-        cursor.execute(query, params)
-        conn.commit()  # Commit the transaction
-        conn.close()
-    except pyodbc.Error as e:
-        print("Database error:", e)
-    except Exception as ex:
-        print("General error:", ex)
+            cursor.execute(query, params)
+            conn.commit()
+            conn.close()
+        except pyodbc.Error as e:
+            print("Database error:", e)
+        except Exception as ex:
+            print("General error:", ex)
+        time.sleep(5)
 
 if __name__ == "__main__":
     cli()
