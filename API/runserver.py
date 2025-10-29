77b23379fed6a92615b7bbaff5394b360f9f93b5
James Stephens <onlineworkmail@gmail.com>
2025-10-29 19:38:29 -0400

    Removing unused imports

--- API/runserver.py
+++ API/runserver.py
@@ -1,10 +1,5 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask
 from flask_cors import CORS
-import platform
-import pyodbc
-from metric_modules import get_cpu_usage, get_rsnapshots, get_uptime, get_all_services_status, get_service_status
-from functions.module1 import get_db_connection
-
 from route.get_cpu_usage_history import get_cpu_usage_history
 from route.index import index
 from route.get_metrics import get_metrics
@@ -24,7 +19,6 @@ app.add_url_rule('/api/get-servers', view_func=get_servers)
 app.add_url_rule('/api/hostname', view_func=get_hostname)
 app.add_url_rule('/api/rsnap', view_func=get_rsnap)
 app.add_url_rule('/api/metrics', view_func=set_metrics)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
